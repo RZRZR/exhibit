7c5bf89aa5909529c374db8fa0a0b3b19abaafde
Rachel Rayns <rachel@raspberrypi.org>
2015-09-22 15:33:45 +0100

    Update pi.py

--- pi.py
+++ pi.py
@@ -5,22 +5,17 @@ import random
 
 connected = False
 
-ser = serial.Serial("/dev/ttyACM0", 9600, timeout=3.0)
+ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=3.0) # Arduino controlling DOTSTAR
+ser2 = serial.Serial("/dev/ttyACM0", 9600, timeout=3.0) # Arduino reading 16 POTs
 ser.write("1")
+ser2.write("1")
 sleep(1)
 
 sockets = [1, 2, 3, 4]
 
 
-while True:
-    scoreString = input("Enter number:  ")
-    scorePrint = "%03d" %scoreString
-    # "{0:0=3d}".format(scoreString)
-    print "input %s" %scorePrint
-    ser.write(str(scorePrint))
-    score = int(float(scoreString))
-    print score
-    switch_off(0)
+
+def shuffle():
     random.shuffle(sockets) # sockets is now e.g [4, 2, 1, 3]
     sockets_1 = sockets[:1] # sockets_1 is now [4]
 
@@ -29,6 +24,31 @@ while True:
 
     random.shuffle(sockets) # sockets is now e.g [3, 1, 2, 4]
     sockets_3 = sockets[:3] # sockets_3 is now [3, 1, 2]
+    
+
+while True:
+    
+    # Enter a number between 0 - 100 - turn it into a 3 digit number.    
+    scoreString = input("Enter number:  ")
+    scoreThreeDigit = "%03d" %scoreString
+    print "input %s" %scoreThreeDigit
+    
+    # if button is pressed
+    
+    # Read score from Arduino 2
+    
+    
+    
+    # Send score to Arduino 1 as a string
+    ser.write(str(scoreThreeDigit))
+    
+    # Print score as a int
+    score = int(float(scoreString))
+    print score
+
+    #
+    shuffle()
+
 
     if score <= 25:
         print "Turn on 1 light"
@@ -40,23 +60,18 @@ while True:
         print "Turn on 2 lights"
         for socket in sockets_2:
             switch_on(socket)
-            sleep(1)
             print "socket %d" % socket
 
     if score >= 51 and score <= 75:
         print "Turn on 3 lights"
         for socket in sockets_3:
             switch_on(socket)
-            sleep(1)
             print "socket %d" % socket
 
     if score >= 76:
         print "Turn on 4 lights"
         switch_on(0)
-    sleep(15)
-    switch_on(0)
-
-
+    sleep(5)
 
 
 ## close the port and end the program
